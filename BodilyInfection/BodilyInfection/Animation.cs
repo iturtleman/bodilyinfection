9081538d784a4822f3ebd72bea5103c0dae43b89
djnemec <djnemec@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-02 01:28:04 +0000

    Ported Animation class

--- BodilyInfection/BodilyInfection/Animation.cs
+++ BodilyInfection/BodilyInfection/Animation.cs
@@ -2,10 +2,114 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
 
 namespace BodilyInfection
 {
+
+    /// <summary>
+    /// This class loads sets of images into
+    /// an animation set.
+    /// </summary>
     class Animation
     {
+        #region Properties
+        public int Built { get; set; }
+        public int NumFrames { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region Variables
+        private string spritePath = "ArtAssets/Sprites/";
+        public List<SpriteFrame> Frames = new List<SpriteFrame>();
+        #endregion
+
+        #region Constructors
+        public Animation(string filename):
+            this(filename, filename)
+        {
+        }
+
+        public Animation(string filename, string name)
+        {
+            LoadAnimation(filename);
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Loads the animations from a file.
+        /// </summary>
+        /// <param name="filename"></param>
+        private void LoadAnimation(string filename){
+            string buffer;
+            string file;
+            int[] transparency = new int[3];
+            int pause;
+            filename = spritePath + filename;
+
+            using (StreamReader stream = new StreamReader(filename))
+            {
+                int count = 0;
+                while(!stream.EndOfStream)
+                {
+                    buffer = stream.ReadLine();
+
+                    // ...
+                    Built = 1;
+
+                    if (buffer.Length > 0 &&
+                        !new char[] { 
+                            '#', '\r', '\0', '\n' 
+                        }.Contains(buffer[0]) &&
+                        !buffer.StartsWith("NumFrames:"))
+                    {
+                        string[] frameInfo = buffer.Split('\t');
+                        file = frameInfo[0];
+                        pause = Int32.Parse(frameInfo[1]);
+                        for (int x = 0; x < 3; x++)
+                        {
+                            int value = Int32.Parse(frameInfo[1 + x]);
+                            if (value >= 0 && value <= 255)
+                            {
+                                transparency[x] = value;
+                            }
+                            else
+                            {
+                                transparency[x] = 0;
+                            }
+                            float pegX = 0;
+                            float pegY = 0;
+                            float.TryParse(frameInfo[5], out pegX);
+                            float.TryParse(frameInfo[6], out pegY);
+
+                            /** sets frame delay and makes sure height and width are correct */
+				            Frames[count].Pause = pause;
+				            Width = Frames[count].Width; 
+				            Height = Frames[count].Height;
+				
+				            /** Set the animation Peg*/
+				            Frames[count].AnimationPeg = new Vector2(pegX + (float)Width/2, pegY + (float)Height/2);
+
+				            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool operator ==(Animation x, Animation y)
+        {
+            return x.Name == y.Name;
+        }
+
+        public static bool operator !=(Animation x, Animation y)
+        {
+            return x.Name != y.Name;
+        }
+        #endregion
     }
 }
---
+++ BodilyInfection/BodilyInfection/SpriteFrame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace BodilyInfection
+{
+    class SpriteFrame
+    {
+        #region Properties
+        /// <summary>
+        /// Image Texture
+        /// </summary>
+        public Texture2D image { get; set; }
+
+        /// <summary>
+        /// Amount of time to pause between
+        /// this frame and next.
+        /// </summary>
+        public int Pause { get; set; }
+
+        /// <summary>
+        /// Base width of the frame's image.
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Base height of the frame's image.
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// The offeset from position to place
+        /// the image. Defaults to (0,0)
+        /// </summary>
+        public Vector2 AnimationPeg { get; set; }
+        #endregion
+
+        #region Variables
+        /// <summary>
+        ///  Hot spots that can be used for
+        ///  locating objects on the sprite
+        ///  default is tagged to center
+        ///  of the sprite
+        /// </summary>
+        public List<Vector2> HotSpots = new List<Vector2>();
+
+        /// <summary>
+        /// The collision data for this sprite.
+        /// </summary>
+        public List<Collision> CollisionData = new List<Collision>();
+        #endregion
+    }
+}
