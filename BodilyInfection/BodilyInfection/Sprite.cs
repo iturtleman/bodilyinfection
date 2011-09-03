8b8b55049f4d0480b70f3f027d86c22c2853ed60
iturtleman128@gmail.com <iturtleman128@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-03 05:50:23 +0000

    forgot to save before commit

--- BodilyInfection/BodilyInfection/Sprite.cs
+++ BodilyInfection/BodilyInfection/Sprite.cs
@@ -87,7 +87,7 @@ namespace BodilyInfection
             SpriteFrame frame = GetAnimation();
             if (mAnimating == true)
             {
-                if (mLastUpdate.TotalGameTime.Milliseconds + frame.Pause * mSpeed < gameTime.TotalGameTime.Milliseconds)
+                if (mLastUpdate.TotalGameTime.Ticks + frame.Pause * (int)mSpeed < gameTime.TotalGameTime.Ticks)
                 {
                     //obtain current peg 
                     Vector2 ppos = frame.AnimationPeg;
