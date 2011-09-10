8e4615c801912079525b7295b4566e4d223f5661
iturtleman128@gmail.com <iturtleman128@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-10 03:15:45 +0000

    Can move edit frame.

--- Tools/AnimationEditor/AnimationEditor/EditFrame.xaml.cs
+++ Tools/AnimationEditor/AnimationEditor/EditFrame.xaml.cs
@@ -18,15 +18,92 @@ namespace AnimationEditor
 	/// </summary>
 	public partial class EditFrame : UserControl
 	{
+        private bool mouseDownInDataPopup;
+        private Point DragOffset;
+
 		public EditFrame()
 		{
 			this.InitializeComponent();
-		}
+            AddHandlers();
+        }
 
         public EditFrame(Frame f)
         {
             DataContext = f;
             this.InitializeComponent();
+            AddHandlers();
+        }
+
+        public void AddHandlers()
+        {
+            //handles mouse down so mouse up can be handled
+            ButtonClose.MouseLeftButtonDown += new MouseButtonEventHandler(ButtonClose_MouseLeftButtonDown);
+            ButtonClose.MouseLeftButtonUp += new MouseButtonEventHandler(ButtonClose_MouseLeftButtonUp);
+        }
+
+        private void OnDataPopupClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource.GetType() == typeof(Border))
+            {
+                mouseDownInDataPopup = true;
+                DragOffset = e.GetPosition(LayoutRoot);
+                border1.CaptureMouse();
+            }
+        }
+
+        private void OnDataPopupMoving(object sender, MouseEventArgs e)
+        {
+            if (mouseDownInDataPopup)
+            {
+                // we want to move it based on the position of the mouse
+                moveUserControl(e);
+            }
+        }
+
+        private void moveUserControl(MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(LayoutRoot);
+            Double newX = LocalTranslateTransform.X + (mousePos.X - DragOffset.X);
+            Double newY = LocalTranslateTransform.Y + (mousePos.Y - DragOffset.Y);
+            LocalTranslateTransform.X = newX;
+            LocalTranslateTransform.Y = newY;
+        }
+
+        private void OnDataPopupReleased(object sender, MouseButtonEventArgs e)
+        {
+            mouseDownInDataPopup = false;
+            border1.ReleaseMouseCapture();
+        }
+
+        void ButtonClose_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        public void ButtonClose_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            Grid parent = (Parent as Grid);
+            if (parent != null)
+            {
+                parent.Children.Remove(this);
+            }
         }
 	}
+    public class FilenameClipper : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string s = value as string;
+            if(s!=null)
+            return s.Substring(s.LastIndexOf("\\"));
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
 }
