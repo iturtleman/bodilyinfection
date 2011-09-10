2131750f8b26133ecbbaafc2b51dfb92317350bc
iturtleman128@gmail.com <iturtleman128@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-10 08:17:25 +0000

    Completed Basic working version of the animation editor. Next step is to change how animations are loaded.

--- Tools/AnimationEditor/AnimationEditor/storage.cs
+++ Tools/AnimationEditor/AnimationEditor/storage.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.ObjectModel;
 
 namespace AnimationEditor
 {
     public static class storage
     {
         public static MainWindow mainWindow { get; set; }
+
     }
 }
