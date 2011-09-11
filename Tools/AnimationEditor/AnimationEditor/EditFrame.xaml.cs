0e15773fdf3c2dd20ef5480bda59d5756d786650
iturtleman128@gmail.com <iturtleman128@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-11 11:26:30 +0000

    Animation editor has all base features complete.

--- Tools/AnimationEditor/AnimationEditor/EditFrame.xaml.cs
+++ Tools/AnimationEditor/AnimationEditor/EditFrame.xaml.cs
@@ -13,17 +13,17 @@ using System.Windows.Shapes;
 
 namespace AnimationEditor
 {
-	/// <summary>
-	/// Interaction logic for EditFrame.xaml
-	/// </summary>
-	public partial class EditFrame : UserControl
-	{
+    /// <summary>
+    /// Interaction logic for EditFrame.xaml
+    /// </summary>
+    public partial class EditFrame : UserControl
+    {
         private bool mouseDownInDataPopup;
         private Point DragOffset;
 
-		public EditFrame()
-		{
-			this.InitializeComponent();
+        public EditFrame()
+        {
+            this.InitializeComponent();
             AddHandlers();
         }
 
@@ -34,11 +34,185 @@ namespace AnimationEditor
             AddHandlers();
         }
 
+        #region Methods
         public void AddHandlers()
         {
             //handles mouse down so mouse up can be handled
             ButtonClose.MouseLeftButtonDown += new MouseButtonEventHandler(ButtonClose_MouseLeftButtonDown);
             ButtonClose.MouseLeftButtonUp += new MouseButtonEventHandler(ButtonClose_MouseLeftButtonUp);
+            DrawingScreen.MouseDown += new MouseButtonEventHandler(DrawingScreen_MouseDown);
+            DrawingScreen.MouseUp += new MouseButtonEventHandler(DrawingScreen_MouseUp);
+            MouseMove += new MouseEventHandler(EditFrame_MouseMove);
+            SelectAnimPeg.MouseUp += new MouseButtonEventHandler(SelectAnimationPeg);
+            AddRect.MouseUp += new MouseButtonEventHandler(AddRect_MouseUp);
+            AddCirc.MouseUp += new MouseButtonEventHandler(AddCirc_MouseUp);
+            Loaded += new RoutedEventHandler(EditFrame_Loaded);
+        }
+
+        public void DrawCollisions()
+        {
+            CollisionData.Children.Clear();
+            Frame f = DataContext as Frame;
+            foreach (var item in f.Collisions)
+            {
+                if (item.GetType() == typeof(CollisionCircle))
+                {
+                    CollisionCircle c = item as CollisionCircle;
+                    Ellipse circle = new Ellipse()
+                    {
+                        Width = c.radius,
+                        Height = c.radius,
+                        Stroke = Brushes.Green,
+                        StrokeThickness = 2
+                    };
+
+                    Canvas.SetTop(circle, c.center.Y - c.radius/2);
+                    Canvas.SetLeft(circle, c.center.X - c.radius/2);
+
+                    CollisionData.Children.Add(circle);
+                }
+                else if (item.GetType() == typeof(CollisionRect))
+                {
+                    CollisionRect r = item as CollisionRect;
+                    Rectangle rectangle = new Rectangle
+                    {
+                        Width = r.Width,
+                        Height = r.Height,
+                        Stroke = Brushes.Green,
+                        StrokeThickness = 2
+                    };
+
+                    Canvas.SetLeft(rectangle, r.TL.X);
+                    Canvas.SetTop(rectangle, r.TL.Y);
+
+                    CollisionData.Children.Add(rectangle);
+                }
+            }
+            Ellipse el = new Ellipse
+            {
+                Stroke = Brushes.Yellow,
+                StrokeThickness = 5,
+                Width = 5,
+                Height = 5
+            };
+            Canvas.SetLeft(el, f.AnimationPeg.X);
+            Canvas.SetTop(el, f.AnimationPeg.Y);
+            CollisionData.Children.Add(el);
+        }
+        #endregion Methods
+
+        #region Event Handlers
+        void EditFrame_Loaded(object sender, RoutedEventArgs e)
+        {
+            Frame f = DataContext as Frame;
+            f.Collisions.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(Collisions_CollectionChanged);
+            DrawCollisions();
+        }
+
+        void Collisions_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            DrawCollisions();
+        }
+
+        void AddCirc_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AddingCircle = true;
+            DrawingScreen.Visibility = Visibility.Visible;
+        }
+
+        void AddRect_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AddingRectangle = true;
+            DrawingScreen.Visibility = Visibility.Visible;
+        }
+
+        void EditFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+                return;
+
+            var pos = e.GetPosition(DrawingScreen);
+
+            if (rect != null)
+            {
+                var x = Math.Min(pos.X, StartPoint.X);
+                var y = Math.Min(pos.Y, StartPoint.Y);
+
+                var w = Math.Max(pos.X, StartPoint.X) - x;
+                var h = Math.Max(pos.Y, StartPoint.Y) - y;
+
+                rect.Width = w;
+                rect.Height = h;
+
+                Canvas.SetLeft(rect, x);
+                Canvas.SetTop(rect, y);
+            }
+
+            else if (circ != null)
+            {
+                var radius = (pos - StartPoint).Length;
+
+                circ.Width = radius;
+                circ.Height = radius;
+
+                Canvas.SetLeft(circ, StartPoint.X - radius / 2);
+                Canvas.SetTop(circ, StartPoint.Y - radius / 2);
+            }
+        }
+
+        void DrawingScreen_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            Frame f = (DataContext as Frame);
+            if (SelectPeg)
+            {
+                f.AnimationPeg = ActualPos;
+                SelectPeg = false;
+            }
+            else if (rect != null)
+            {
+                f.Collisions.Add(new CollisionRect(ActualPos, rect.Width, rect.Height));
+                rect = null;
+                AddingRectangle = false;
+            }
+            else if (circ != null)
+            {
+                f.Collisions.Add(new CollisionCircle(ActualPos, circ));
+                circ = null;
+                AddingCircle = false;
+            }
+            DrawingScreen.Children.Clear();
+            DrawingScreen.Visibility = Visibility.Collapsed;
+            DrawCollisions();
+        }
+
+        void DrawingScreen_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            StartPoint = e.GetPosition(DrawingScreen);
+            ActualPos = e.GetPosition(FrameImage);
+            if (AddingRectangle)
+            {
+                rect = new Rectangle
+                {
+                    Stroke = Brushes.Green,
+                    StrokeThickness = 2
+                };
+                Canvas.SetLeft(rect, StartPoint.X);
+                Canvas.SetRight(rect, StartPoint.Y);
+                DrawingScreen.Children.Add(rect);
+            }
+            else if (AddingCircle)
+            {
+                circ = new Ellipse
+                {
+                    Stroke = Brushes.Green,
+                    StrokeThickness = 2
+                };
+                Canvas.SetLeft(circ, StartPoint.X);
+                Canvas.SetTop(circ, StartPoint.Y);
+                DrawingScreen.Children.Add(circ);
+            }
         }
 
         private void OnDataPopupClicked(object sender, MouseButtonEventArgs e)
@@ -89,7 +263,29 @@ namespace AnimationEditor
                 parent.Children.Remove(this);
             }
         }
-	}
+
+        private void SelectAnimationPeg(object sender, MouseButtonEventArgs e)
+        {
+            SelectPeg = true;
+            DrawingScreen.Visibility = Visibility.Visible;
+        }
+        #endregion Event Handlers
+
+        public bool AddingRectangle;
+
+        public bool AddingCircle;
+
+        public bool SelectPeg;
+
+        public Point StartPoint = new Point();
+
+        public Point ActualPos = new Point();
+
+        private Rectangle rect;
+
+        private Ellipse circ;
+
+    }
     public class FilenameClipper : IValueConverter
     {
 
