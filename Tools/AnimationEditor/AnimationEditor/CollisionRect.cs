4989447f6c2637bf8322df4f32aeed86f9a091c1
iturtleman128@gmail.com <iturtleman128@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-12 14:44:24 +0000

    added collision files

---
+++ Tools/AnimationEditor/AnimationEditor/CollisionRect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace AnimationEditor
+{
+    public class CollisionRect : Collision
+    {
+        public Point TL;
+        public double Width;
+        public double Height;
+
+        public CollisionRect(Point p, double w, double h)
+        {
+            // TODO: Complete member initialization
+            TL = p;
+            Width = w;
+            Height = h;
+        }
+
+        public override XElement GetLine()
+        {
+            XElement e = new XElement("Collision");
+            e.SetAttributeValue("Type", "Rectangle");
+            e.SetAttributeValue("TL", TL);
+            e.SetAttributeValue("Width", Width);
+            e.SetAttributeValue("Height", Height);
+            return e;
+        }
+    }
+}
