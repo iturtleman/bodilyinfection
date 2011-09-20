2c21da0ae4a212189da1bb802552f5572b7d5653
bruce@thebevilacquas.com <bruce@thebevilacquas.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-20 01:53:35 +0000

    Created tool to programmatically add boundaries to anim files

---
+++ trunk/Tools/BoundaryCreator/BoundaryCreator/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace BoundaryCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pointsFile = new System.Windows.Forms.Button();
+            this.animPreFile = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(828, 411);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // pointsFile
+            // 
+            this.pointsFile.Location = new System.Drawing.Point(12, 12);
+            this.pointsFile.Name = "pointsFile";
+            this.pointsFile.Size = new System.Drawing.Size(409, 23);
+            this.pointsFile.TabIndex = 1;
+            this.pointsFile.Text = "Open Collision Points File (.colpoints)";
+            this.pointsFile.UseVisualStyleBackColor = true;
+            this.pointsFile.Click += new System.EventHandler(this.pointsFile_Click);
+            // 
+            // animPreFile
+            // 
+            this.animPreFile.Location = new System.Drawing.Point(431, 12);
+            this.animPreFile.Name = "animPreFile";
+            this.animPreFile.Size = new System.Drawing.Size(409, 23);
+            this.animPreFile.TabIndex = 2;
+            this.animPreFile.Text = "Open AnimPre File (.animpre)";
+            this.animPreFile.UseVisualStyleBackColor = true;
+            this.animPreFile.Click += new System.EventHandler(this.animPreFile_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(431, 465);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(409, 23);
+            this.save.TabIndex = 3;
+            this.save.Text = "Save Output Anim File (.anim)";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(13, 465);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(408, 23);
+            this.generate.TabIndex = 4;
+            this.generate.Text = "Generate";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 500);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.animPreFile);
+            this.Controls.Add(this.pointsFile);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button pointsFile;
+        private System.Windows.Forms.Button animPreFile;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button generate;
+    }
+}
+
---
+++ trunk/Tools/BoundaryCreator/BoundaryCreator/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace BoundaryCreator
+{
+    public partial class Form1 : Form
+    {
+        List<Tuple<float, float>> polygonPoints = new List<Tuple<float, float>>();
+        List<Tuple<float, float, float, float, float>> separatedPoints = new List<Tuple<float, float, float, float, float>>();
+        IEnumerable<string> animPre = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            animPreFile.Enabled = false;
+            generate.Enabled = false;
+            save.Enabled = false;
+        }
+
+        private void pointsFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Collision Points Files|*.colpoints";
+            openFileDialog1.Title = "Select a Collision Points File";
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.ShowDialog();
+
+            string file = openFileDialog1.FileName;
+            IEnumerable<string> lines = new List<string>();
+            try
+            {
+                lines = File.ReadLines(file);
+
+                bool endPolygon = false;
+
+                foreach (string line in lines)
+                {
+                    if ((line[0] == '\\' && line[1] == '\\') || line.Length == 0)
+                        continue;
+                    if (line[0] == '#')
+                    {
+                        endPolygon = true;
+                        continue;
+                    }
+
+                    if (endPolygon)
+                    {
+                        string[] points = line.Split(';');
+                        string[] point1 = points[0].Split(',');
+                        string[] point2 = points[1].Split(',');
+                        separatedPoints.Add(new Tuple<float, float, float, float, float>(float.Parse(point1[0]), float.Parse(point1[1]), float.Parse(point2[0]), float.Parse(point2[1]), float.Parse(points[2])));
+                    }
+                    else
+                    {
+                        string[] point = line.Split(',');
+                        polygonPoints.Add(new Tuple<float, float>(float.Parse(point[0]), float.Parse(point[1])));
+                    }
+                }
+
+                textBox1.Clear();
+                animPreFile.Enabled = true;
+                generate.Enabled = false;
+                save.Enabled = false;
+            }
+            catch {}
+        }
+
+
+        private void animPreFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "AnimPre Files|*.animpre";
+            openFileDialog1.Title = "Select an AnimPre File";
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.ShowDialog();
+
+            string file = openFileDialog1.FileName;
+
+            try
+            {
+                animPre = File.ReadLines(file);
+                generate.Enabled = true;
+            }
+            catch {}
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            //Create collision text
+            string collisionText = "";
+            int count = polygonPoints.Count;
+            for (int i = 0; i < count; i++)
+            {
+                collisionText += "    <Collision Type=\"OBB\" Corner1=";
+                collisionText += "\"" + polygonPoints[i].Item1.ToString("0.0####") + "," + polygonPoints[i].Item2.ToString("0.0####") + "\"";
+                collisionText += " Corner2=";
+                collisionText += "\"" + polygonPoints[(i + 1) % count].Item1.ToString("0.0####") + "," + polygonPoints[(i + 1) % count].Item2.ToString("0.0####") + "\"";
+                collisionText += " Thickness=\"20.0\"/>" + Environment.NewLine;
+            }
+
+            count = separatedPoints.Count;
+            for (int i = 0; i < count; i++)
+            {
+                collisionText += "    <Collision Type=\"OBB\" Corner1=";
+                collisionText += "\"" + separatedPoints[i].Item1.ToString("0.0####") + "," + separatedPoints[i].Item2.ToString("0.0####") + "\"";
+                collisionText += " Corner2=";
+                collisionText += "\"" + separatedPoints[i].Item3.ToString("0.0####") + "," + separatedPoints[i].Item4.ToString("0.0####") + "\"";
+                collisionText += " Thickness=\"" + separatedPoints[i].Item5.ToString("0.0####") + "\"/>" + Environment.NewLine;
+            }
+
+            int index = 10000000;
+            textBox1.Text = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine + "<Animation>" + Environment.NewLine;
+            foreach (string frame in animPre)
+            {
+                if (!frame.Contains("<Frame"))
+                    continue;
+
+                int index1 = frame.Trim().IndexOf("/>");
+                int index2 = frame.Trim().IndexOf(">");
+                if (index1 != -1)
+                    index = index1;
+                if (index2 != -1)
+                    index = Math.Min(index, index2);
+                if (index == 10000000)
+                    textBox1.Text = "Bad Formatting";
+                else
+                {
+                    string formattedFrame = frame.Trim().Remove(index-1);
+                    textBox1.Text += "  " + formattedFrame + ">" + Environment.NewLine;
+                    textBox1.Text += collisionText;
+                    textBox1.Text += "  </Frame>" + Environment.NewLine;
+                }
+            }
+
+            if (index != 10000000)
+            {
+                textBox1.Text += "</Animation>";
+                save.Enabled = true;
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Anim File | .anim";
+            saveFileDialog1.Title = "Save an Anim File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                // NOTE that the FilterIndex property is one-based.
+
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(textBox1.Text);
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+        }
+    }
+}
