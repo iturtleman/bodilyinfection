ace495cee027b8f910755b437b9166aaa1942de6
vincentgarcia00@gmail.com <vincentgarcia00@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-20 05:58:47 +0000

    Added stage clear and lungs levels. Right now stage clear always takes you to stage 2 when you press the start button, even if you just completed stage 2.

--- BodilyInfection/BodilyInfection/LoadResources.cs
+++ BodilyInfection/BodilyInfection/LoadResources.cs
@@ -8,9 +8,11 @@ namespace BodilyInfection
     internal partial class Game : Microsoft.Xna.Framework.Game
     {
         void LoadResources(){
+            mLevels.Add(new Level("stageclear", Levels.StageClear.Load, Levels.StageClear.Update, Levels.StageClear.Unload, Levels.StageClear.CompletionCondition));
             mLevels.Add(new Level("TitleScreen", Levels.TitleScreen.Load, Levels.TitleScreen.Update, Levels.TitleScreen.Unload, Levels.TitleScreen.CompletionCondition));
             mLevels.Add(new Level("GameOver", Levels.GameOver.Load, Levels.GameOver.Update, Levels.GameOver.Unload, Levels.GameOver.CompletionCondition));
             mLevels.Add(new Level("World", Levels.Level1.Load, Levels.Level1.Update, Levels.Level1.Unload, Levels.Level1.CompletionCondition));
+            mLevels.Add(new Level("level2", Levels.Level2.Load, Levels.Level2.Update, Levels.Level2.Unload, Levels.Level2.CompletionCondition));
         }
     }
 }
