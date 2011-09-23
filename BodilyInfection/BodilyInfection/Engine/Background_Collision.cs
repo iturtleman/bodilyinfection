1f16fcc8b617ee1d300faf4e5eedbe05bd0d9216
iturtleman128@gmail.com <iturtleman128@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-23 06:34:52 +0000

    now it really works XP

---
+++ BodilyInfection/BodilyInfection/Engine/Background_Collision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BodilyInfection
+{
+    class Background_Collision : WorldObject
+    {
+        public Background_Collision(CollisionObject col)
+        {
+            Col = col;
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override List<CollisionObject> GetCollision()
+        {
+            return new List<CollisionObject>() { Col };
+        }
+    }
+}
--- BodilyInfection/BodilyInfectionContent/Backgrounds/n.py
+++ BodilyInfection/BodilyInfectionContent/Backgrounds/n.py
@@ -3,8 +3,8 @@ lines = f.readlines()
 f.close()
 w = 2027
 f = open("new.txt", 'w')
-lines = lines[5:52]
+lines = lines[5:46]
 for line in reversed(lines):
-	x, y = map(int, line.split(','))
+	x, y = map(int, line.rstrip('\n').split(','))
 	f.write(str(w - x) + ',' + str(y) + '\n')
 f.close()
