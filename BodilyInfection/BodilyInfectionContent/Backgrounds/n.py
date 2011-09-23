ca9a3c04be635165e4dc25add00039d820d9dbd7
iturtleman128@gmail.com <iturtleman128@gmail.com@be696562-eace-ce61-25ea-7a97913d95db>
2011-09-23 07:12:05 +0000

    updated some stuff

--- BodilyInfection/BodilyInfectionContent/Backgrounds/n.py
+++ BodilyInfection/BodilyInfectionContent/Backgrounds/n.py
@@ -3,7 +3,7 @@ lines = f.readlines()
 f.close()
 w = 2027
 f = open("new.txt", 'w')
-lines = lines[5:46]
+lines = lines[5:50]
 for line in reversed(lines):
 	x, y = map(int, line.rstrip('\n').split(','))
 	f.write(str(w - x) + ',' + str(y) + '\n')
